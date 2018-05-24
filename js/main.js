643d89b0e88583ab53ceae4cc9831470c19f20c8
tlin98 <tonylin514@gmail.com>
2018-05-23 18:29:36 -0700

    Update registrationRequest data

--- js/main.js
+++ js/main.js
@@ -90,7 +90,7 @@ $(function(){
                     "Cache-Control": "no-cache",
                 },
                 processData: false,
-                data: regStartObj
+                data: JSON.stringify(regStartObj);
             }
 
             getChallengeRequest = $.ajax(settings);
